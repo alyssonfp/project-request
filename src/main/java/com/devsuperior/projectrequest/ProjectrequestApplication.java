ed429f0f1783de28df8ea9550d26c18e23b71527
alyssonfp <alyssonfp@gmail.com>
2021-07-01 15:36:33 -0300

    first commit

---
+++ src/main/java/com/devsuperior/projectrequest/ProjectrequestApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectrequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectrequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/projectrequest/ProjectrequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectrequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
